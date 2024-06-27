ff78c57b7e680d36988af545fe58efac2cdc6222
Vitor Moschetti <vitormoschetti@terra.com.br>
2024-06-26 21:12:42 -0300

    service portfolio

--- service-log/src/test/java/br/com/log/ServiceLogApplicationTests.java
+++ service-log/src/test/java/br/com/log/ServiceLogApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.portfolio;
+package br.com.log;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServicePortfolioApplicationTests {
+class ServiceLogApplicationTests {
 
     @Test
     void contextLoads() {
